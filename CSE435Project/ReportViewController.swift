0900fccffebe50edcb991d43275554bec97873a7
SchoonSauce <adamschoonmaker12@gmail.com>
2015-12-02 15:53:23 -0500

    gives visual display while sending csv data, notifies of error with alert updates "Send" button for sending csv data

--- CSE435Project/ReportViewController.swift
+++ CSE435Project/ReportViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class ReportViewController: UIViewController {
 
+    @IBOutlet weak var sendingOverlay: UIView!
+    @IBOutlet weak var sendingPopup: UIView!
+    
+    
     @IBOutlet weak var saveButton: UIBarButtonItem!
     @IBOutlet weak var spacerButton: UIBarButtonItem!
+    @IBOutlet weak var sendButton: UIBarButtonItem!
     
     @IBOutlet weak var carLeftView: UIView!
     @IBOutlet weak var carLeftImageView: UIImageView!
@@ -32,17 +37,15 @@ class ReportViewController: UIViewController {
     
     var analysis: Analysis!
     
+    private var overlayHasBeenVisibleForMinimumAmountOfTime = false
+    private var csvDataUploadAttemptHasCompleted = false
+    
     @IBAction func onBackButton(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
     @IBAction func onSendButton(sender: AnyObject) {
-        let url = NSURL(string: "https://www.cse.msu.edu/~cse435/Projects/F2015/Groups/Team6/receive_data/receiveData.php")
-        let request = NSMutableURLRequest(URL: url!, cachePolicy: NSURLRequestCachePolicy.UseProtocolCachePolicy, timeoutInterval: 60)
-        request.HTTPMethod = "POST"
-        request.HTTPBody = "data=\(analysis.toCsvString())".dataUsingEncoding(NSUTF8StringEncoding)
-        let connection = NSURLConnection(request: request, delegate: nil)
-        connection!.start()
+        uploadCsvData()
     }
 
     
@@ -62,13 +65,118 @@ class ReportViewController: UIViewController {
         saveButton.enabled = false
     }
     
+    private func uploadCsvData() {
+        let config = NSURLSessionConfiguration.ephemeralSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        
+        let url = NSURL(string: "https://www.cse.msu.edu/~cse435/Projects/F2015/Groups/Team6/receive_data/receiveData.php")
+        let request = NSMutableURLRequest(URL: url!)
+        request.HTTPMethod = "POST"
+        let csvData = "data=\(analysis.toCsvString())".dataUsingEncoding(NSUTF8StringEncoding)
+        
+        onCsvUploadTaskAttempt()
+        
+        let uploadTask = session.uploadTaskWithRequest(request, fromData: csvData) { [weak self] data, response, error in
+            guard let strongSelf = self else { return }
+            dispatch_async(dispatch_get_main_queue(), {
+                strongSelf.onCsvUploadTaskAttemptCompletion(data, response: response, error: error)
+            })
+        }
+        
+        uploadTask.resume()
+    }
+    
+    // actions to perform before attempting to upload csv
+    private func onCsvUploadTaskAttempt() {
+        animateSendingOverlayToBeShown(true)
+        sendButton.enabled = false
+        csvDataUploadAttemptHasCompleted = false
+    }
+    
+    // actions to perform after attempting to upload csv
+    private func onCsvUploadTaskAttemptCompletion(data: NSData?, response: NSURLResponse?, error: NSError?) {
+        csvDataUploadAttemptHasCompleted = true
+        
+        let errorString = "Error posting CSV data to server"
+        
+        guard let response = response as? NSHTTPURLResponse where error == nil else {
+            print(errorString)
+            sendButton.enabled = true
+            hideSendingOverlay()
+            showFailedSendingAlert()
+            return
+        }
+        
+        guard response.statusCode == 200 else {
+            print(errorString + ": HTTP status code not success")
+            sendButton.enabled = true
+            hideSendingOverlay()
+            showFailedSendingAlert()
+            return
+        }
+        
+        print("success posting data")
+        animateSendingOverlayToBeShown(false)
+        sendButton.enabled = false
+    }
+    
+    private func showFailedSendingAlert() {
+        let alertTitle = "Unable To Upload"
+        let alertDismissAction = UIAlertAction(title: "Okay", style: .Default, handler: nil)
+        let alertMessage = "Please try sending again"
+        let alertController = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: .Alert)
+        alertController.addAction(alertDismissAction)
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    private func animateSendingOverlayToBeShown(toBeShown: Bool) {
+        let animationDuration = 0.3
+        let minimumOverlayShowTime = 2.0
+        
+        if toBeShown {
+            overlayHasBeenVisibleForMinimumAmountOfTime = false
+            UIView.animateWithDuration(animationDuration) { [weak self] in
+                guard let strongSelf = self else { return }
+                strongSelf.showSendingOverlay()
+                NSTimer.scheduledTimerWithTimeInterval(minimumOverlayShowTime, target: strongSelf, selector: "minimumOverlayVisibilityTimerHasCompleted", userInfo: nil, repeats: false)
+            }
+        } else if overlayHasBeenVisibleForMinimumAmountOfTime && csvDataUploadAttemptHasCompleted {
+            UIView.animateWithDuration(animationDuration) { [weak self] in
+                guard let strongSelf = self else { return }
+                strongSelf.hideSendingOverlay()
+            }
+        }
+    }
+    
+    func minimumOverlayVisibilityTimerHasCompleted() {
+        overlayHasBeenVisibleForMinimumAmountOfTime = true
+        animateSendingOverlayToBeShown(false)
+    }
+    
+    private func showSendingOverlay() {
+        sendingOverlay.alpha = 0.5
+        sendingPopup.alpha = 1
+    }
+    
+    private func hideSendingOverlay() {
+        sendingOverlay.alpha = 0
+        sendingPopup.alpha = 0
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        hideSendingOverlay()
         
         let barButtonBackgroundHidden = UIImage(named: "barButtonBackgroundHidden")
         spacerButton.setBackgroundImage(barButtonBackgroundHidden, forState: UIControlState.Disabled, barMetrics: UIBarMetrics.Default)
         spacerButton.enabled = false
         
+        let dataFromDefectMarkViewInitialization = initializeDefectMarkViews()
+        initializeReportSummaryLabels(dataFromDefectMarkViewInitialization)
+    }
+    
+    private func initializeDefectMarkViews() -> (numberOfDefects: Int, numberOfUnits: Int) {
         // add the defect mark views for each side as subviews
         carLeftView.addSubview(defectMarkViewLeft)
         carTopView.addSubview(defectMarkViewTop)
@@ -100,16 +208,17 @@ class ReportViewController: UIViewController {
         defectMarkViewTop.updateWithDefects(topSideDefects)
         defectMarkViewRight.updateWithDefects(rightSideDefects)
         
-        // 
-        // get data for report summary labels and set them
-        //
-        
         let numberOfDefects = leftSideDefects.count + topSideDefects.count + rightSideDefects.count
         let numberOfUnits = samples.count
-        let defectsPerUnit = Float(numberOfDefects) / Float(numberOfUnits)
         
-        defectsInReportLabel.text = "Defects in Report: \(numberOfDefects)"
-        unitsInReportLabel.text = "Units in Report: \(numberOfUnits)"
+        return (numberOfDefects, numberOfUnits)
+    }
+    
+    private func initializeReportSummaryLabels(defectData: (numberOfDefects: Int, numberOfUnits: Int)) {
+        let defectsPerUnit = Float(defectData.numberOfDefects) / Float(defectData.numberOfUnits)
+        
+        defectsInReportLabel.text = "Defects in Report: \(defectData.numberOfDefects)"
+        unitsInReportLabel.text = "Units in Report: \(defectData.numberOfUnits)"
         defectsPerUnitLabel.text = "Defects Per Unit: \(defectsPerUnit)"
     }
     
